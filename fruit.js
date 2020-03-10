a4ee0a8060ba859ff6cacdb6697c25b6b4730b65
AFutureWebDev <jenna.gullick2@gmail.com>
2020-03-10 10:13:11 -0500

    created nested elements with groups

---
+++ fruit.js
@@ -0,0 +1,38 @@
+import {fruitBowl} from './fruitBowl.js';
+
+const svg = d3.select("svg");
+
+
+const makeFruit = type => ({
+    type,
+    id: Math.random()
+});
+let fruits = d3.range(5)
+    .map(() => makeFruit('apple'));
+
+const render = () => {
+    fruitBowl(svg, {
+        fruits, 
+        height: +svg.attr('height')
+    })
+};
+
+render();
+
+setTimeout(() => {
+    //Eats an apple.
+    fruits.pop();
+    render();
+}, 1000);
+
+setTimeout(() => {
+    //Replacing an apple with a lemon.
+    fruits[2].type = 'lemon';
+    render();
+}, 2000);
+
+setTimeout(() => {
+    //Eats an apple.
+    fruits = fruits.filter((d, i) => i !== 1);
+    render();
+}, 3000);
---
+++ fruitBowl.js
@@ -0,0 +1,41 @@
+const colorScale = d3.scaleOrdinal()
+.domain(['apple', 'lemon'])
+.range(['#c11d1d', '#eae600']);
+
+const radiusScale = d3.scaleOrdinal()
+.domain(['apple', 'lemon'])
+.range(['80', '50']);
+
+const xPosition = (d, i) => i * 180 + 100;
+
+export const fruitBowl = (selection, props) => {
+    const {fruits, height} = props;
+
+    const bowl = selection.selectAll('rect')
+        .data([null])
+        .enter().append('rect')
+            .attr('y', 110)
+            .attr('width', 920)
+            .attr('height', 300)
+            .attr('rx', 300 / 2);
+
+    const groups = selection.selectAll('g')
+        .data(fruits);
+    const groupsEnter = groups.enter().append('g')
+       groupsEnter
+        .merge(groups)
+                .attr('transform', (d, i) =>
+                    `translate(${i * 180 + 100}, ${height / 2})`);
+    groups.exit().remove();
+
+
+    groupsEnter.append('circle')
+        .merge(groups.select('circle'))
+            .attr('fill', d => colorScale(d.type))
+            .attr('r', d => radiusScale(d.type));
+
+    groupsEnter.append('text')
+        .merge(groups.select('text'))
+            .text(d => d.type)
+            .attr('y', 120)
+}
